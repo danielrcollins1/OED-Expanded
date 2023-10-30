f5d79c67b68f1031d51a0817eb67c764477ef8ae
baquies-since1995 <139177984+baquies-since1995@users.noreply.github.com>
2023-10-30 13:56:49 -0400

    Update makepdf.bat
    added cover

--- makepdf.bat
+++ makepdf.bat
@@ -1,5 +1,5 @@
 REM Windows batch file to compile PDF using Pandoc
 REM Assumes Pandoc has PDF engine (e.g., via MiKTeX install)
 
-pandoc --output=OED-Expanded.pdf --pdf-engine=xelatex --variable mainfont="Arial" --table-of-contents introduction.md core-mechanic.md characters.md equipment.md gameplay.md npcs.md rewards.md monsters.md traps.md resources.md x-additional-material.md x-campaign-material.md x-design-notes.md
+pandoc --output=OED-Expanded.pdf --pdf-engine=xelatex --variable mainfont="Arial" --cover.md table-of-contents introduction.md core-mechanic.md characters.md equipment.md gameplay.md npcs.md rewards.md monsters.md traps.md resources.md x-additional-material.md x-campaign-material.md x-design-notes.md
 
